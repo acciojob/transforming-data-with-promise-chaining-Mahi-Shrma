649eeb24a031f4e3261e49423de85104b6a4f773
Mahi-Shrma <151066048+Mahi-Shrma@users.noreply.github.com>
2024-11-07 15:12:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,301 @@
-//your JS code here. If required.
+// const btn = document.getElementById('btn');
+// const output = document.getElementById('output');
+
+// btn.addEventListener('click', () => {
+//     const number = parseInt(document.getElementById('ip').value);
+
+//     function f1(number) {
+//         return new Promise((resolve) => {
+//             setTimeout(() => {
+//                 resolve(number);
+//             }, 2000);
+//         });
+//     }
+
+//     f1(number)
+//         .then((number) => {
+//             return new Promise((resolve) => {
+//                 setTimeout(() => {
+//                     const result = number;
+//                     output.innerText = `Result: ${result}`;
+//                     resolve(result);
+//                 }, 2000);
+//             });
+//         })
+//         .then((number) => {
+//             return new Promise((resolve) => {
+//                 setTimeout(() => {
+//                     const result = number;
+//                     output.innerText = `Result: ${result}`;
+//                     resolve(result);
+//                 }, 2000);
+//             });
+//         })
+//         .then((number) => {
+//             return new Promise((resolve) => {
+//                 setTimeout(() => {
+//                     const result = number * 2;
+//                     output.innerText = `Result: ${result}`;
+//                     resolve(result);
+//                 }, 1000);
+//             });
+//         })
+//         .then((number) => {
+//             return new Promise((resolve) => {
+//                 setTimeout(() => {
+//                     const result = number - 3;
+//                     output.innerText = `Result: ${result}`;
+//                     resolve(result);
+//                 }, 1000);
+//             });
+//         })
+//         .then((number) => {
+//             return new Promise((resolve) => {
+//                 setTimeout(() => {
+//                     const result = number / 2;
+//                     output.innerText = `Result: ${result}`;
+//                     resolve(result);
+//                 }, 1000);
+//             });
+//         })
+//         .then((number) => {
+//             return new Promise((resolve) => {
+//                 setTimeout(() => {
+//                     const result = number + 10;
+//                     output.innerText = `Result: ${result}`;
+//                     resolve(result);
+//                 }, 1000);
+//             });
+//         })
+//         .then((number) => {
+//             output.innerText = `Final Result: ${number}`;
+//         })
+//         .catch((error) => console.error(error));
+// });
+
+const btn= document.getElementById('btn');
+const output = document.getElementById('output');
+
+btn.addEventListener('click',()=>{
+	const number = parseInt(document.getElementById('ip').value);
+	function f1(number){
+		return new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve(number);
+		},2000)
+	});
+}
+f1(number)
+.then((number)=>{
+	return new Promise((resolve) => {
+            setTimeout(() => {
+                const result = number;
+                output.innerText =`Result: ${result}`;
+                resolve(result);
+            }, 2000);
+        });
+})
+.then((number)=>{
+	return new Promise((resolve) => {
+            setTimeout(() => {
+                const result = number;
+                output.innerText =`Result: ${result}`;
+                resolve(result);
+            }, 2000);
+        });
+})
+.then((number)=>{
+	return new Promise((resolve) => {
+            setTimeout(() => {
+                const result = number*2;
+                output.innerText = `Result: ${result}`;
+                resolve(result);
+            }, 1000);
+        });
+})
+.then((number)=>{
+	return new Promise((resolve) => {
+            setTimeout(() => {
+                const result = number-3;
+                output.innerText =`Result: ${result}`;
+                resolve(result);
+            }, 1000);
+        });
+})
+.then((number)=>{
+	return new Promise((resolve) => {
+            setTimeout(() => {
+                const result = number/2;
+                output.innerText =`Result: ${result}`;
+                resolve(result);
+            }, 1000);
+        });
+})
+.then((number)=>{
+	return new Promise((resolve) => {
+            setTimeout(() => {
+                const result = number+10;
+                output.innerText =`Result: ${result}`;
+                resolve(result);
+            }, 1000);
+        });
+})
+.then((number)=>{
+                output.innerText =`Final Result: ${number}`;
+        }).catch((error) => console.error(error));
+});
+
+
+
+
+
+
+
+// btn.addEventListener('click',f1().
+// then((number)=>{
+// 	return new Promise((resolve) => {
+//             setTimeout(() => {
+//                 const result = number;
+//                 output.innerText = Result: <result>;
+//                 resolve(result);
+//             }, 2000);
+//         });
+// })
+// .then((number)=>{
+// 	return new Promise((resolve) => {
+//             setTimeout(() => {
+//                 const result = number;
+//                 output.innerText = Result: <result>;
+//                 resolve(result);
+//             }, 2000);
+//         });
+// })
+// .then((number)=>{
+// 	return new Promise((resolve) => {
+//             setTimeout(() => {
+//                 const result = number*2;
+//                 output.innerText = Result: <result>;
+//                 resolve(result);
+//             }, 1000);
+//         });
+// })
+// .then((number)=>{
+// 	return new Promise((resolve) => {
+//             setTimeout(() => {
+//                 const result = number-3;
+//                 output.innerText = Result: <result>;
+//                 resolve(result);
+//             }, 1000);
+//         });
+// })
+// .then((number)=>{
+// 	return new Promise((resolve) => {
+//             setTimeout(() => {
+//                 const result = number/2;
+//                 output.innerText = Result: <result>;
+//                 resolve(result);
+//             }, 1000);
+//         });
+// })
+// .then((number)=>{
+// 	return new Promise((resolve) => {
+//             setTimeout(() => {
+//                 const result = number+10;
+//                 output.innerText = Result: <result>;
+//                 resolve(result);
+//             }, 1000);
+//         });
+// })
+// .then((number)=>{
+// 	return new Promise((resolve) => {
+//             setTimeout(() => {
+//                 const result = number;
+//                 output.innerText =Final Result: <result>;
+//                 resolve(result);
+//             }, 0);
+//         });
+// }).catch((error) => console.error(error));
+// );
+
+// function f1(number){
+// 	return new Promise((resolve)=>{
+// 		setTimeout(()=>{
+// 			resolve(number);
+// 		},2000)
+// 	});
+// };
+// f1().
+// then((number)=>{
+// 	return new Promise((resolve) => {
+//             setTimeout(() => {
+//                 const result = number;
+//                 output.innerText = Result: <result>;
+//                 resolve(result);
+//             }, 2000);
+//         });
+// })
+// .then((number)=>{
+// 	return new Promise((resolve) => {
+//             setTimeout(() => {
+//                 const result = number;
+//                 output.innerText = Result: <result>;
+//                 resolve(result);
+//             }, 2000);
+//         });
+// })
+// .then((number)=>{
+// 	return new Promise((resolve) => {
+//             setTimeout(() => {
+//                 const result = number*2;
+//                 output.innerText = Result: <result>;
+//                 resolve(result);
+//             }, 1000);
+//         });
+// })
+// .then((number)=>{
+// 	return new Promise((resolve) => {
+//             setTimeout(() => {
+//                 const result = number-3;
+//                 output.innerText = Result: <result>;
+//                 resolve(result);
+//             }, 1000);
+//         });
+// })
+// .then((number)=>{
+// 	return new Promise((resolve) => {
+//             setTimeout(() => {
+//                 const result = number/2;
+//                 output.innerText = Result: <result>;
+//                 resolve(result);
+//             }, 1000);
+//         });
+// })
+// .then((number)=>{
+// 	return new Promise((resolve) => {
+//             setTimeout(() => {
+//                 const result = number+10;
+//                 output.innerText = Result: <result>;
+//                 resolve(result);
+//             }, 1000);
+//         });
+// })
+// .then((number)=>{
+// 	return new Promise((resolve) => {
+//             setTimeout(() => {
+//                 const result = number;
+//                 output.innerText =Final Result: <result>;
+//                 resolve(result);
+//             }, 0);
+//         });
+// }).catch((error) => console.error(error));
+
+
+
+
+
+
+
+
+
+
+
